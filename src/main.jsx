66d98b5f3670453649d38bd7352b13459b2605dc
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2025-08-28 18:08:38 +0300

    feat: integrate Microsoft Clarity for enhanced user analytics; update dependencies in package.json and package-lock.json

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Clarity from "@microsoft/clarity";
+
+const projectId = "t1y32eg5hi";
+if (import.meta.env.MODE === "production") {
+  Clarity.init(projectId);
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
